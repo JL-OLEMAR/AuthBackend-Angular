d48b2c859fc0e086837594f0b5414c8f1b439737
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-21 14:11:24 -0500

    feat: crear rutas de la applicacion

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+// Crear el servidor/aplicación de express
+const app = express()
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en http://localhost:${4000}`)
+})
