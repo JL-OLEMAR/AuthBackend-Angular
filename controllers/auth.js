212ac3125cbf878ffe6281345b2f6da59b150be2
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-21 14:53:52 -0500

    feat: configurar CORS y body de las peticiones

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,25 @@
 const { response } = require('express')
 
 const crearUsuario = (req, res = response) => {
+  const { name, email, password } = req.body
+  console.log(name, email, password)
+
   return res.json({
     ok: true,
     msg: 'Crear usuario /new'
   })
 }
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body
+  console.log(email, password)
   return res.json({
     ok: true,
     msg: 'Login de usuario /'
   })
 }
 
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
   return res.json({
     ok: true,
     msg: 'Renew'
