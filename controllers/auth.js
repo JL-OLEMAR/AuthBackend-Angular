f23aec2371a9eb220a6983f47ee8549a4465e61f
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-22 16:23:36 -0500

    feat: validar campos obligatorios

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express')
+const { validationResult } = require('express-validator')
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body
@@ -11,6 +12,15 @@ const crearUsuario = (req, res = response) => {
 }
 
 const loginUsuario = (req, res = response) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+  }
+
   const { email, password } = req.body
   console.log(email, password)
   return res.json({
