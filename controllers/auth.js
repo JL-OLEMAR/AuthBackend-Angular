00592897b9d52dd9351d9bb39aebe1b8f4d57b08
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-22 23:00:43 -0500

    feat: obtimizar la validacion de campos through un middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,7 @@
 const { response } = require('express')
-const { validationResult } = require('express-validator')
 
 const crearUsuario = (req, res = response) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    })
-  }
-
   const { name, email, password } = req.body
-  console.log(name, email, password)
 
   return res.json({
     ok: true,
@@ -21,17 +10,7 @@ const crearUsuario = (req, res = response) => {
 }
 
 const loginUsuario = (req, res = response) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    })
-  }
-
   const { email, password } = req.body
-  console.log(email, password)
   return res.json({
     ok: true,
     msg: 'Login de usuario /'
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos.js')
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
 
 const router = Router()
@@ -8,13 +9,15 @@ const router = Router()
 router.post('/new', [
   check('name', 'El nombre es obligatorio').not().isEmpty(),
   check('email', 'Agrega un email válido').isEmail(),
-  check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 })
+  check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 }),
+  validarCampos
 ], crearUsuario)
 
 // Login de usuario
 router.post('/', [
   check('email', 'El email es obligatorio').isEmail(),
-  check('password', 'La contraseña es obligatoria').isLength({ min: 6 })
+  check('password', 'La contraseña es obligatoria').isLength({ min: 6 }),
+  validarCampos
 ], loginUsuario)
 
 // Validar y revalidar token
