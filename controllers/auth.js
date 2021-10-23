358efecffe2cfbc160d1f3f1dac966d9af4d4ae9
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-23 03:02:00 -0500

    feat: crear usuario en la db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,42 @@
 const { response } = require('express')
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
   const { name, email, password } = req.body
 
-  return res.json({
-    ok: true,
-    msg: 'Crear usuario /new'
-  })
+  try {
+    // Verificar el email
+    const usuario = await Usuario.findOne({ email })
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario ya existe con ese email'
+      })
+    }
+
+    // Crear usuario con el modelo
+    const dbUser = new Usuario(req.body)
+
+    // Hashear el password
+
+    // Generar el JWT
+
+    // Crear el usuario de DB
+    await dbUser.save()
+
+    // Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser._id,
+      name
+    })
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: 'Por favor, comuniquese con el administrador'
+    })
+  }
 }
 
 const loginUsuario = (req, res = response) => {
