14be2e9e9b985e89c0987861b49b5bea1af76199
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-12 16:53:51 -0500

    add files para build

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 require('dotenv').config()
 const dbConnection = require('./db/config.js')
 
@@ -20,6 +21,11 @@ app.use(express.json())
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'))
+
+// Manejar demas rutas
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${process.env.PORT}`)
